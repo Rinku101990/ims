bc7170243f6d3bde5d5376e729148f974c1322f4
Rinku Vishwakarma <Rinku101990@gmail.com>
2018-09-03 00:55:13 +0530

    stud_section_0309
    student notification

--- application/controllers/students/Dashboard.php
+++ application/controllers/students/Dashboard.php
@@ -36,14 +36,44 @@ class Dashboard extends CI_Controller {
 
 		$data['profile'] = $this->sam->get_students_profile_record($stu_id,$stu_role);
 		$data['menus'] = $this->sam->get_menu_role_permissions($stu_role);
+		//print_r($data);die;
+		$class_section = $this->sam->get_student_class_section_by_master_id($stu_id);
 
-		// echo "<pre>";
-		// print_r($data);die;
+		// $notifiArray = array(
+		// 	'' => $class_section->stud_id,
+		// 	'' => $class_section->stud_id,
+		// 	'' => $class_section->stud_id,
+		// 	'' => $class_section->stud_id,
+		// );
+		$stdid  = $class_section->stud_id;
+		$roleid = $class_section->roles_id;
+		$clsid  = $class_section->cls_id;
+		$sectid = $class_section->sect_id;
+
+		$data['notify'] = $this->sam->get_student_unread_notification_list($stdid,$roleid,$clsid,$sectid);
+
+		echo "<pre>";
+		print_r($data);die;
 
 		$this->load->view('students/includes/header',$data);
 		$this->load->view('students/includes/sidebar');
 		$this->load->view('students/includes/top_header');
 		$this->load->view('students/index');
 		$this->load->view('students/includes/footer');
+	}
+	// GET LOGIN STUDENT DETAIL WITH CLASS AND SECTION //
+	//public function 
+	// VIEW ALL UNREAD NOTIFICATION //
+	public function new_unread_notification()
+	{
+	    $reference = $this->session->userdata('logged_in');
+			if(empty($reference)){
+				redirect('','refresh');
+			}
+	    // $login_id = $reference['cms_id'];
+	    // print_r($login_id);
+	    $unread = $this->input->post('unread');
+	    $result['message'] = $this->sam->get_all_unread_notification($unread);
+	    echo json_encode($result);
 	}
 }
--- application/models/Students_activities_model.php
+++ application/models/Students_activities_model.php
@@ -14,7 +14,16 @@ class Students_activities_model extends CI_Model {
 		//echo $this->db->last_query();
 		return $query->row();
 	}
-
+	// GET STUDENTS CLASS SECTION //
+	public function get_student_class_section_by_master_id($stu_id)
+	{
+		$this->db->select('stud_id,roles_id,cls_id,sect_id');
+		$this->db->from('cms_students');
+		$this->db->where('cms_id',$stu_id);
+		$query = $this->db->get();
+		//echo $this->db->last_query();
+		return $query->row();
+	}
 
 	// GET MENU LIST BASED ON ROLE PERMISSION
 	public function get_menu_role_permissions($stu_role)
@@ -29,11 +38,24 @@ class Students_activities_model extends CI_Model {
 	}
 
 	// GET ALL UNREAD NOTIFICATION //
-	public function get_all_unread_notification($unread)
+	// public function get_all_unread_notification($unread)
+	// {
+	// 	$this->db->select('COUNT(ntfn_notification_read_status) as message');		
+	// 	$query = $this->db->get('cms_notifications');
+	// 	$this->db->where('ntfn_notification_read_status',$unread);
+	// 	$count = $query->row_array();
+	// 	return $count;
+	// }
+	// GET ALL UNREAD NOTIFICATION //
+	public function get_student_unread_notification_list($stdid,$roleid,$clsid,$sectid)
 	{
 		$this->db->select('COUNT(ntfn_notification_read_status) as message');		
 		$query = $this->db->get('cms_notifications');
-		$this->db->where('ntfn_notification_read_status',$unread);
+		$this->db->where('stud_id',$stdid);
+		$this->db->where('roles_id',$roleid);
+		$this->db->where('cls_id',$clsid);
+		$this->db->where('sect_id',$sectid);
+		$this->db->where('ntfn_notification_read_status','0');
 		$count = $query->row_array();
 		return $count;
 	}
